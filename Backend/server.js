449f0158624349827697006fcb44358c6961830f
Wissarut Kanasub <wissarut.dev@gmail.com>
2024-04-14 21:09:24 +0700

    Create Connection to MySQL server

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,33 @@
+// import library and config files.
 const express = require("express");
-const mysql = require("mysql")
+const mysql = require("mysql");
+const config = require("./config");
 
 const app = express();
 
-// MySQL connection
-const connection = mysql.createConnection( {
-    host: '',
-    user: '',
-    password: '',
-    database: '',
+// MySQL connection.
+const connection = mysql.createConnection(config.database);
+connection.connect((error) => {
+    if (error) {
+        console.log("Error connection to database server:", error)
+        return null;
+    }
+    console.log("Connection to database server successfully.")
 });
 
+// Route or Path.
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    const query = "SELECT * FROM weathersense_gather";
+    connection.query(query, (error, results, fields) => {
+        if (error) {
+            console.log("Error executing query:", error);
+            res.status(500).json({ error: "Error executing query" });
+            return null;
+        }
+        res.json(results);
+    });
 });
 
-const port = 3000
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(config.port, () => {
+    console.log(`Server is running on http://localhost:${config.port}`);
 });
