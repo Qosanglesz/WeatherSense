1acf353ec2dfe1d69f513668624e802847b04e9f
Wissarut Kanasub <wissarut.dev@gmail.com>
2024-04-15 18:04:33 +0700

    Sent data to predict at modelAPI server complete

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const config = require("./config");
+const axios = require("axios");
 
 const app = express();
 
@@ -14,6 +15,42 @@ connection.connect((error) => {
     }
     console.log("Connection to database server successfully.")
 });
+
+// Function.
+function loadUnPredictData(callback) {
+    const query = "SELECT * FROM weathersense_gather";
+    connection.query(query, (error, results, fields) => {
+        if (error) {
+            console.log("Error executing query:", error);
+            callback(error, null);
+            return null;
+        }
+        callback(null, results);
+    });
+}
+function predictAndSaveData() {
+    loadUnPredictData((error, results) => {
+        if (error) {
+            // Handle error
+            console.error("Error loading unpredicted data:", error);
+            return null;
+        }
+        // Handle results
+        // Sent data to ModelAPI-server
+        axios.post(config.modelPath.predictPath, results)
+            .then((response) => {
+                // Handle successful response
+                console.log('Predicted response:', response.data);
+                // Add this line to print the JSON result from the ModelAPI
+                console.log('ModelAPI JSON result:', response.data);
+            })
+            .catch((error) => {
+                // Handle error
+                console.error('Error:', error);
+            });
+
+    });
+}
 
 // Route or Path.
 app.get('/', (req, res) => {
