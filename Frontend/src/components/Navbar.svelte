90132dfa6c8425ccb3bcb30a6f6841430cb3ff51
Wissarut Kanasub <wissarut.dev@gmail.com>
2024-04-20 03:21:34 +0700

    create first page

---
+++ Frontend/src/components/Navbar.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let link;
+</script>
+
+<nav class="bg-gray-800 p-4">
+    <div class="max-w-7xl mx-auto flex justify-between items-center">
+        <!-- Logo -->
+        <div class="text-white font-bold text-2xl">
+            WeatherSense
+        </div>
+        <!-- Navigation Links -->
+        <ul class="flex space-x-4 text-xl">
+            <li>
+                <a href="{link}" class="text-white hover:text-gray-300">Home</a>
+            </li>
+            <li>
+                <a href="{link}" class="text-white hover:text-gray-300">About</a>
+            </li>
+            <li>
+                <a href="{link}" class="text-white hover:text-gray-300">Services</a>
+            </li>
+            <li>
+                <a href="{link}" class="text-white hover:text-gray-300">Contact</a>
+            </li>
+        </ul>
+    </div>
+</nav>
--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,4 +1,27 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
+<script>
+    import Card from "../components/Card.svelte";
+    import Navbar from "../components/Navbar.svelte";
 
+    let link = {};
+
+</script>
+
+<Navbar link={link}/>
+
+<div class="mx-4">
+    <div class="grid grid-cols-2 gap-4">
+        <Card title="Sensor" value="30 degree Celsius"/>
+        <Card title="API" value="30 degree Celsius"/>
+    </div>
+    <div class="grid grid-cols-3 gap-4">
+        <Card title="Sensor" value="30 degree Celsius"/>
+        <Card title="API" value="30 degree Celsius"/>
+        <Card title="API" value="30 degree Celsius"/>
+        <Card title="Sensor" value="30 degree Celsius"/>
+        <Card title="API" value="30 degree Celsius"/>
+        <Card title="API" value="30 degree Celsius"/>
+        <Card title="Sensor" value="30 degree Celsius"/>
+        <Card title="API" value="30 degree Celsius"/>
+        <Card title="API" value="30 degree Celsius"/>
+    </div>
+</div>
