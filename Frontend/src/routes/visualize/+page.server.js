d3693568b833c8f3159c6b85ee2474969734c105
Wissarut Kanasub <wissarut.dev@gmail.com>
2024-05-07 23:41:17 +0700

    create Histogram

--- Frontend/src/routes/visualize/+page.server.js
+++ Frontend/src/routes/visualize/+page.server.js
@@ -21,6 +21,13 @@ export async function load() {
                 weather_pred: []
             };
 
+            const rangeLabel = {
+                temp: [[10, 15], [16, 20], [21, 25], [26, 30], [31, 35], [36, 40], [41, 45], [46, 50]],
+                commonPercent: [[1, 20], [21, 40], [41, 60], [61, 80], [81, 100]],
+                pressure: [[1001, 1003], [1004, 1007], [1008, 1011], [1012, 1015]],
+                windSpeed: [[0, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]]
+            }
+
             response.data.forEach(entry => {
                 Object.keys(mergedData).forEach(key => {
                     mergedData[key].push(entry[key]);
@@ -30,6 +37,7 @@ export async function load() {
             return {
                 weather : response.data,
                 mergedData: mergedData,
+                rangeLabel: rangeLabel,
             }
         }
     }catch (error) {
--- Frontend/src/routes/visualize/+page.svelte
+++ Frontend/src/routes/visualize/+page.svelte
@@ -7,9 +7,20 @@
     import {onMount} from 'svelte';
     import {Line} from 'svelte-chartjs';
     import LineChart from "../../components/LineChart.svelte";
+    import Histogram from "../../components/Histogram.svelte";
+    import TimestampHistory from "../../components/TimestampHistory.svelte";
 
 </script>
 
-<div>
-    <LineChart name="Temperature From API"  dataArray={data.mergedData.temp_sensor}/>
+<div class="grid grid-cols-4 gap-2">
+    <Histogram name="Temperature Sensor °C" dataArray={data.mergedData.temp_sensor} rangeLabel={data.rangeLabel.temp}/>
+    <Histogram name="Temperature API °C" dataArray={data.mergedData.temp_api} rangeLabel={data.rangeLabel.temp}/>
+    <Histogram name="Cloudiness Percent" dataArray={data.mergedData.cloudiness} rangeLabel={data.rangeLabel.commonPercent}/>
+    <Histogram name="Humidity Sensor Percent" dataArray={data.mergedData.humidity_sensor} rangeLabel={data.rangeLabel.commonPercent}/>
+    <Histogram name="Humidity API Percent" dataArray={data.mergedData.humidity_api} rangeLabel={data.rangeLabel.commonPercent}/>
+    <Histogram name="Wind Speed m/s" dataArray={data.mergedData.wind_speed} rangeLabel={data.rangeLabel.windSpeed}/>
+    <Histogram name="Pressure HectoPascal" dataArray={data.mergedData.pressure} rangeLabel={data.rangeLabel.pressure}/>
 </div>
+
+
+<LineChart name="Temperature From API"  dataArray={data.mergedData.temp_sensor}/>
